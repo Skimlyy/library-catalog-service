58b867c6ea004711fd4cc76bcfbf2dbc349415d5
Skimlyy <kimsvensson96@gmail.com>
2026-03-02 14:39:17 +0100

    Lade till models

---
+++ Library-Catalog-Service/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Library_Catalog_Service.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = "";
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public int ItemTypeId { get; set; }
+    public ItemType? ItemType { get; set; }
+}
---
+++ Library-Catalog-Service/Models/ItemType.cs
@@ -0,0 +1,9 @@
+﻿namespace Library_Catalog_Service.Models;
+
+public class ItemType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+
+    public List<Item> Items { get; set; } = new();
+}
