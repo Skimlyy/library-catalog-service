e7e58969bff10608301b3272d509b95f5ef4a2c9
Skimlyy <kimsvensson96@gmail.com>
2026-03-02 14:55:55 +0100

    Lade till DbContext

---
+++ Library-Catalog-Service/Data/CatalogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Library_Catalog_Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Catalog_Service.Data;
+
+public class CatalogDbContext : DbContext
+{
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options) { }
+
+    public DbSet<Item> Items => Set<Item>();
+    public DbSet<ItemType> ItemTypes => Set<ItemType>();
+}
